86bb95ea0aaa6aa487d01b97851a70cc322eddb3
kraus-s <kraus.sven@gmail.com>
2024-12-10 10:36:01 +0100

    Adressed oversights during the name change
    Made changes ensure everything is on brand now.

--- src/Home.py
+++ src/Home.py
@@ -2,8 +2,8 @@ import streamlit as st
 from lib.constants import IMAGE_HOME
 
 
-st.title("Welcome to Sammlung Toole")
+st.title("Welcome to MÍMIR")
 st.write("The Menu on the left has all the options")
 st.image(IMAGE_HOME)
 st.markdown("For more information, check the help page, or view the " +
-            "[documentation](https://arbeitsgruppe-digitale-altnordistik.github.io/Sammlung-Toole/).")
+            "[documentation](https://arbeitsgruppe-digitale-altnordistik.github.io/mimir/).")
